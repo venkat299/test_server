4f053a0e57d7da89d9bc9c0188257137d92785b0
Venkatesh Duraiarasan <dev_venkat@outlook.com>
2016-05-15 21:23:47 +0530

    added mongodb for testing

--- server.js
+++ server.js
@@ -1,17 +1,23 @@
-
 var seneca = require('seneca')();
-// =========== test env db ==========//
-seneca.use('mongo-store',{
-  name:'harvest_test',
-  host:'127.0.0.1',
-  port:27017
-})
- 
-// =================================//
+seneca.use('entity')
 
-  
+var Promise = require('bluebird')
+
+//Promise.promisify(seneca.make$,{context:seneca})
+//Promise.promisify(seneca.list$,{context:seneca})
+
+
+var config = require('./config.json')
+	//=========== test env db ========== //
+seneca.use(config.test.db_type, config.test.db_config)
+	//================================= //
+
+
 seneca.use('../harvest_data/index.js')
 seneca.use('../harvest_strategy/index.js')
 seneca.use('../harvest_evaluator/index.js')
 
-seneca.listen();
+
+seneca.ready(function() {
+	seneca.listen()
+})
